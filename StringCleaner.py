262669d4f86129bd3013739252bc5df616920840
rafaell <rafaell.casella@instacarro.com>
2019-01-24 09:26:49 -0200

    Created a class to work with CSV files, by reading it and writing.

---
+++ StringCleaner.py
@@ -0,0 +1,29 @@
+"""
+Class created to stringfy inputs, lower-case strings, switch special characters
+and question marks.
+
+"""
+import re
+
+from unidecode import unidecode
+
+
+def clean_str(my_str):
+    if isinstance(my_str, bool):
+        if my_str:
+            my_str = 'TRUE'
+        else:
+            my_str = 'FALSE'
+
+    if isinstance(my_str, int) or isinstance(my_str, float):
+        my_str = str(my_str)
+
+    my_str = my_str.lower()
+    my_str = my_str.replace("  ", " ").strip()
+    my_str = unidecode.unidecode(my_str)
+
+    my_str = re.sub(r'\001', '', my_str)
+    my_str = re.sub(r"[^A-Za-z0-9///' ]", '', my_str)
+    my_str = my_str.replace("  ", " ").strip()
+
+    return my_str
