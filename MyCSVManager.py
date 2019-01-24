b37cdc7af9a14a832b6b11f822306c87acb0a318
rafaell <rafaell.casella@instacarro.com>
2019-01-24 08:54:06 -0200

    Created a class to work with CSV files, by reading it and writing.

---
+++ MyCSVManager.py
@@ -0,0 +1,31 @@
+"""
+Class created to easily work with CSV files.
+
+"""
+
+
+class MyCSVManager:
+    working_directory = "C:/Users/rafae/Desktop"
+    delimiter = ','
+    all_lines = []
+
+    def __init__(self, working_directory):
+        self.working_directory = working_directory
+
+    def __init__(self):
+        pass
+
+    def open(self, file_name) -> list:
+        with open(self._working_directory + file_name, errors='ignore') as \
+                csv_file:
+            self.all_lines = csv_file.readlines()
+            # Just in case the file is semicolon separated
+            if ';' in self.all_lines[0]:
+                for i in range(len(self.all_lines)):
+                    self.all_lines[i] = self.all_lines[i].split(';')
+            return self.all_lines
+
+    def save_lines(self, file_name):
+        with open(file_name + '.csv', mode='w') as out_file:
+            for line in self.all_lines:
+                out_file.write(self.delimiter.join(line))
